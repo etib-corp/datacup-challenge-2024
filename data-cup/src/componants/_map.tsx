2836c4c10097f7e23926b7fcd7b6411f0f65b2be
Lauret Benjamin <benjamin.lauret@epitech.eu>
2024-11-23 15:25:46 +0400

    delete: picture

--- data-cup/src/componants/_map.tsx
+++ data-cup/src/componants/_map.tsx
@@ -8,7 +8,16 @@ export const getData = async () => {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        console.log('Data fetched:', data.results.length, data.total_count);
+        while (data.results.length != (data.total_count - 100)) {
+            offset = (parseInt(offset) + 100).toString();
+            apiUrl = 'https://data.tco.re/api/explore/v2.1/catalog/datasets/signalements_depots_sauvages_citoyennes_10_2024/records?limit=100&offset=' + offset;
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const newData = await response.json();
+            data.results = data.results.concat(newData.results);
+        };
         return data;
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -25,8 +34,15 @@ export const convertToGeoJSON = (data: any) => {
                 geometry: {
                     type: "Point",
                     coordinates: [item.geom.lon, item.geom.lat],
-                    image_name: item.image_name,
                 },
+                image_name: item.image_name,
+                location: {
+                    city: item.ville,
+                    district: item.commune,
+                    street: item.rue
+                },
+                date: item.date,
+                status: item.status
             };
         })
     };
